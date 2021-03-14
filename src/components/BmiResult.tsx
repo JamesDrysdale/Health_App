1c2406fe40a96ee8b471c5ff7fcee700401523cb
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-03-14 11:34:22 +0000

    BMI result now easier to read and understand by rounding to 2 decimal places

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -7,7 +7,7 @@ const BmiResult: React.FC<{BmiResult: number}> = props => {
 			<IonCol>
 				<IonCard>
 					<IonCardContent>
-						<h2>{props.BmiResult}</h2>
+						<h2>{props.BmiResult.toFixed(2)}</h2>
 					</IonCardContent>
 				</IonCard>
 			</IonCol>
